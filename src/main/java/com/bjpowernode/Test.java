53936d336cba4464a3cb90e174010097442b0ba0
yasur <794865095@qq.com>
2019-12-10 11:40:21 +0800

    this test

---
+++ src/main/java/com/bjpowernode/Test.java
@@ -0,0 +1,11 @@
+package com.bjpowernode;
+
+/**
+ * Author:794865095@qq.com
+ * 2019-12-10
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是原始测试文件");
+    }
+}
